76bd42ef3ad14a8f77f9f0a4ca1bd035587cf2db
Josh Owens <me@jowens.dev>
2022-06-26 10:03:19 -0500

    setting up recording links

--- ui/src/layout/index.js
+++ ui/src/layout/index.js
@@ -6,7 +6,11 @@ function Layout ({ children }) {
   return (
     <div>
       <Header />
-      {children}
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          {children}
+        </div>
+      </div>
     </div>
   )
 }
--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+// import { VideoCameraIcon } from '@heroicons/react/solid'
 
 export default function Home () {
   return (
-    <h1 className="text-3xl font-bold underline">
-            Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+              Hello world!
+      </h1>
+      <a
+        className="mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center rounded-xl border-2 border-dashed border-blue-400 bg-white p-6 text-center">
+        <span className='ml-2 h-10 w-10 text-3xl'>
+              &#x1F37A;
+        </span>
+        <p className="mt-2 text-gray-500 tracking-wide">Click to record you first drink!</p>
+      </a>
+      <a
+        className="mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center rounded-xl border-2 border-dashed border-blue-400 bg-white p-6 text-center">
+        <span className='ml-2 h-10 w-10 text-3xl'>
+            &#x1F37B;
+        </span>
+        <p className="mt-2 text-gray-500 tracking-wide">Click to record you last drink!</p>
+      </a>
+    </>
   )
 }

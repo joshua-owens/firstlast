d0217ad6a49001caf545cb90ae21a22e0d980427
Josh Owens <me@jowens.dev>
2022-06-25 22:38:31 -0500

    tailwind

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,12 +4,16 @@ import VideoRecorder from 'react-video-recorder'
 function App() {
   return (
     <div className="App">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <VideoRecorder
           onRecordingComplete={(videoBlob) => {
             // Do something with the video...
             console.log('videoBlob', videoBlob)
           }}
           timeLimit={10000}
+          isOnInitially={true}
         />
     </div>
   );
